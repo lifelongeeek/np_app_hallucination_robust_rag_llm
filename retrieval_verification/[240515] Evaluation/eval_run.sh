bfc88006197383dcb0ce1d201b0a9f658d2c2110
Jaeyeon Kim <0310kjy@gmail.com>
2024-05-14 16:06:35 +0900

    add evaluation code

---
+++ retrieval_verification/[240515] Evaluation/eval_run.sh
@@ -0,0 +1,11 @@
+CUDA_VISIBLE_DEVICES=0 python eval_prob.py \
+    --model_path /root/plav_train/output/llama_3_4k/bs4_acc4/checkpoint-800
+
+# CUDA_VISIBLE_DEVICES=0 python eval_prob.py \
+#     --model_path /root/plav_train/output/llama_3_4k/bs4_acc4/checkpoint-1000
+
+# CUDA_VISIBLE_DEVICES=0 python eval_prob.py \
+#     --model_path /root/plav_train/output/llama_3_4k/bs4_acc4/checkpoint-1200
+
+# CUDA_VISIBLE_DEVICES=0 python eval_prob.py \
+#     --model_path /root/plav_train/output/llama_3_4k/bs4_acc4/checkpoint-1400
